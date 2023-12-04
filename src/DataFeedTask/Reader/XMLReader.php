aab48acc0e7c12c304434280fb150dd173546ab9
awaisahmedsukhera <awaisahmed10071995@gmail.com>
2023-12-04 20:18:22 +0100

    Improvements in reader and writer constructor

--- src/DataFeedTask/Reader/XMLReader.php
+++ src/DataFeedTask/Reader/XMLReader.php
@@ -8,21 +8,24 @@ use Monolog\Handler\StreamHandler;
 class XMLReader implements Reader
 {
     private $logger;
+    private $filePath;
 
-    public function __construct()
+    public function __construct($filePath)
     {
+        $this->filePath = $filePath;
+
         // Creating a Monolog logger instance with a stream handler
         $this->logger = new Logger('xml_reader');
         $this->logger->pushHandler(new StreamHandler('/home/awais/myphpproject/logs/error.log', Logger::ERROR));
     }
 
-    public function readData($filePath)
+    public function readData()
     {
         try {
-            $tree = simplexml_load_file($filePath);
+            $tree = simplexml_load_file($this->filePath);
             return $tree;
         } catch (\Exception $e) {
-            $errorMessage = "Error parsing XML in file '{$filePath}': {$e->getMessage()}";
+            $errorMessage = "Error parsing XML in file '{$this->filePath}': {$e->getMessage()}";
             $this->logger->error($errorMessage);
             return null;
         }
